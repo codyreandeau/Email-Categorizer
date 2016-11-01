e933f47c7571162711de1cae951776d25af3a2ea
Cody Reandeau <codyreandeau@yahoo.com>
2016-11-01 15:13:36 -0400

    Find some code to be redundant. Specifically, the code that was used for splitting the text into paragraphs and then using textwrap to format the message text. Also wrote in the modules im using

--- extract_email_text.py
+++ extract_email_text.py
@@ -4,6 +4,10 @@ characters, /xa0 characters, then tokenizes
 the words, which are stored in a set
 '''
 
+import nltk, re
+from nltk.corpus import stopwords
+from bs4 import BeautifulSoup
+
 def extract_message(url):
 	markup = open(url)
 	soup = BeautifulSoup(markup, "html.parser")
@@ -12,10 +16,8 @@ def extract_message(url):
 	text = soup.get_text()
 	text_clean = re.sub(r"\n", " ", text)
 	text_clean = text_clean.replace(u'\xa0', u' ')
-	paras = text_clean.split("\n\n")
-	formatted = "\n\n".join(textwrap.fill(p) for p in paras)
-	a = formatted.find('From:')
-	tokens = word_tokenize((formatted[a:]))
+	a = text_clean.find('From:')
+	tokens = word_tokenize((text_clean[a:]))
 	tokens_set = set(tokens)
 	stopwords = nltk.corpus.stopwords.words('english')
 	content = [w for w in tokens_set if w.lower() not in stopwords]
