81a56d5f8891c25e346e96706d57909e5fc3a459
Cody Reandeau <codyreandeau@yahoo.com>
2016-11-27 22:37:59 -0500

    Added my own punctuation list. Added regex in method to eliminate numbers and unfamiliar alien characters

--- extract_email_text.py
+++ extract_email_text.py
@@ -3,16 +3,22 @@ from nltk.corpus import stopwords
 from bs4 import BeautifulSoup
 from nltk import word_tokenize
 
+#Punctuation list
+punct = [".",";",":","!","?","/","\\",",","#","@","$","&",")","...",
+"''","<",">","(","\"!","@","#","$","%","^","&","*"]
+
 def extract_message(url):
 	markup = open(url)
 	soup = BeautifulSoup(markup, "html.parser")
 	for script in soup(["script", "style"]):
 		script.extract()
 	text = soup.get_text()
+	text = re.sub(r'[^\x00-\x7F]+','', text) #Eliminate Foreign Characters
+	text = re.sub(r'\d+', '', text) #Eliminate Numbers
 	a = text.find('From:')
 	tokens = word_tokenize((text[a:]))
 	tokens_set = set(tokens)
 	stopwords = nltk.corpus.stopwords.words('english')
 	content = [w for w in tokens_set if w.lower() not in stopwords]
-	content = [p for p in content if p not in string.punctuation]
+	content = [p for p in content if p not in punct]
 	return content
