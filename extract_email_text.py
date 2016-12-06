870b586212c208d42d47233194744ad60524c9c8
Cody Reandeau <codyreandeau@yahoo.com>
2016-12-05 23:00:40 -0500

    Using sklearn for classification. Used the 20newsgroup dataset to train my classifier. Tested a few of my emails and they were assigned the appropriate labels

--- extract_email_text.py
+++ extract_email_text.py
@@ -1,24 +1,82 @@
+#-------------------------------------------------------------------------------
+# Name:        module4
+# Purpose:
+#
+# Author:      Cody
+#
+# Created:     28/11/2016
+# Copyright:   (c) Cody 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
 import nltk, re, string
 from nltk.corpus import stopwords
 from bs4 import BeautifulSoup
 from nltk import word_tokenize
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 
-#Punctuation list
+categories = ['comp.graphics', 'soc.religion.christian', 'misc.forsale',
+'talk.politics.misc', 'sci.electronics']
 punct = [".",";",":","!","?","/","\\",",","#","@","$","&",")","...",
 "''","<",">","(","\"!","@","#","$","%","^","&","*"]
+news = fetch_20newsgroups(categories=categories)
 
 def extract_message(url):
-	markup = open(url)
-	soup = BeautifulSoup(markup, "html.parser")
-	for script in soup(["script", "style"]):
-		script.extract()
-	text = soup.get_text()
-	text = re.sub(r'[^\x00-\x7F]+','', text) #Eliminate Foreign Characters
-	text = re.sub(r'\d+', '', text) #Eliminate Numbers
-	a = text.find('From:')
-	tokens = word_tokenize((text[a:]))
-	tokens_set = set(tokens)
-	stopwords = nltk.corpus.stopwords.words('english')
-	content = [w for w in tokens_set if w.lower() not in stopwords]
-	content = [p for p in content if p not in punct]
-	return content
+    markup = open(url)
+    soup = BeautifulSoup(markup, "html.parser")
+    for script in soup(["script", "style"]):
+        script.extract()
+    text = soup.get_text()
+    text = re.sub(r'[^\x00-\x7F]+',' ', text) #Eliminate Foreign Characters
+    text = re.sub(r'\d+', ' ', text)
+    text = re.sub(r'\W+', ' ', text)
+    a = text.find('View')
+    final_text = text[a:]
+    return final_text
+
+gary = extract_message("C:\\Users\\Cody\\Documents\\Emails\\gary.html")
+gary2 = extract_message("C:\\Users\\Cody\\Documents\\Emails\\gary2.html")
+jesus = extract_message("C:\\Users\\Cody\\Documents\\Emails\\Jesus.html")
+jesus2 = extract_message("C:\\Users\\Cody\\Documents\\Emails\\shop.html")
+
+print (news.target_names)
+print ()
+
+count_vect = CountVectorizer()
+x_train_counts = count_vect.fit_transform(news.data)
+x_train_counts.shape
+count_vect.vocabulary_.get(u'algorithm')
+
+tfidf_transformer = TfidfTransformer()
+x_train_tfidf = tfidf_transformer.fit_transform(x_train_counts)
+x_train_tfidf.shape
+
+tf_transformer = TfidfTransformer(use_idf=False).fit(x_train_counts)
+x_train_tf = tf_transformer.transform(x_train_counts)
+x_train_tf.shape
+
+clf = MultinomialNB().fit(x_train_tfidf, news.target)
+
+docs_new = [gary, gary2, jesus, jesus2]
+
+x_new_counts = count_vect.transform(docs_new)
+x_new_tfidf = tfidf_transformer.transform(x_new_counts)
+
+predicted = clf.predict(x_new_tfidf)
+
+for doc, category in zip(docs_new, predicted):
+     print('%r ----> %s' % (doc, news.target_names[category]))
+     print()
+     print()
